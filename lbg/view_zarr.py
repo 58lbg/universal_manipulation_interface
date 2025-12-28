689271d12cecc46ca9ad624eb719a7162fc54f1c
zhangke05 <zhangke05@58.com>
2025-12-28 16:40:08 +0800

    测试普通相机检测开闭情况

--- lbg/view_zarr.py
+++ lbg/view_zarr.py
@@ -2,5 +2,5 @@ import zarr
 from diffusion_policy.codecs.imagecodecs_numcodecs import register_codecs, JpegXl
 register_codecs()
 
-root = zarr.open('../dataset/dp_train_data.zarr.zip')
+root = zarr.open('./dataset/dp_train_data.zarr.zip')
 print(root.tree())
