9082754aceadacb64b48a657270ab4ba5138a47c
zhangke05 <zhangke05@58.com>
2026-01-04 10:18:42 +0800

    测试普通相机检测开闭情况

--- scripts_slam_pipeline/02_create_map.py
+++ scripts_slam_pipeline/02_create_map.py
@@ -27,7 +27,7 @@ from umi.common.cv_util import draw_predefined_mask
 @click.option('-m', '--map_path', default=None, help='ORB_SLAM3 *.osa map atlas file')
 @click.option('-d', '--docker_image', default="chicheng/orb_slam3:latest")
 @click.option('-np', '--no_docker_pull', is_flag=True, default=False, help="pull docker image from docker hub")
-@click.option('-nm', '--no_mask', is_flag=True, default=False, help="Whether to mask out gripper and mirrors. Set if map is created with bare GoPro no on gripper.")
+@click.option('-nm', '--no_mask', is_flag=True, default=True, help="Whether to mask out gripper and mirrors. Set if map is created with bare GoPro no on gripper.")
 def main(input_dir, map_path, docker_image, no_docker_pull, no_mask):
     video_dir = pathlib.Path(os.path.expanduser(input_dir)).absolute()
     for fn in ['raw_video.mp4', 'imu_data.json']:
