01255a744a283b54e0f8c4cc168c24e1c5d098e2
zhangke05 <zhangke05@58.com>
2026-01-04 10:37:26 +0800

    测试普通相机检测开闭情况

--- scripts/detect_aruco.py
+++ scripts/detect_aruco.py
@@ -79,7 +79,7 @@ def main(input, output, intrinsics_json, aruco_yaml, num_workers):
             results.append(result)
 
     # dump
-    print(results)
+    #print(results)
     pickle.dump(results, open(os.path.expanduser(output), 'wb'))
 
 
