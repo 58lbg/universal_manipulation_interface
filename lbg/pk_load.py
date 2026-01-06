2853c60111dc309eac2ea946608fa894eae3a9db
zhangke05 <zhangke05@58.com>
2026-01-06 17:06:26 +0800

    修改镜头畸变文件

--- lbg/pk_load.py
+++ lbg/pk_load.py
@@ -2,6 +2,6 @@ import os
 import pickle
 
 base_dir = os.path.dirname(__file__)
-tag_detection = os.path.join(base_dir, "../100GOPRO/demos/100GOPRO/demos/gripper_calibration_C3441350089206_2026.01.06_15.21.15.536983/tag_detection.pkl")
+tag_detection = os.path.join(base_dir, "../100GOPRO/demos/gripper_calibration_C3441350089206_2026.01.06_15.21.15.536983/tag_detection.pkl")
 tag_detection_results = pickle.load(open(tag_detection, 'rb'))
 print(tag_detection_results)
